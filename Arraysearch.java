f5b4e11fc236176d6bdd609161b3029ca1543832
Sreelakshmi29-2002 <sreelakshmipandikasalavalappil@gmail.com>
2025-03-21 15:34:30 +0530

    Add files via upload

---
+++ Arraysearch.java
@@ -0,0 +1,43 @@
+public class Arraysearch 
+{
+int[] array;
+public Arraysearch(int[] array)
+{
+this.array=array;
+}
+public int searchElement(int target)
+{
+for(int i=0;i<array.length;i++)
+{
+if(array[i] == target)
+{
+return i;
+}
+}
+return -1;
+}
+public void displayArray()
+{
+for(int num : array)
+{
+System.out.println(num + " ");
+}
+System.out.println();
+}
+public static void main(String[] args)
+{
+int[] arr={1,2,3,9,5,6,19};
+Arraysearch searchobj=new Arraysearch(arr);
+searchobj.displayArray();
+int target =9;
+int result=searchobj.searchElement(target);
+if(result !=1) 
+{
+System.out.println("Element "+target+ "found at index" +result);
+} 
+else
+{
+System.out.println("Element "+target+ "not found at index" +result);
+}
+}
+}
